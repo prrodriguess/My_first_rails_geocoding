c0611143d75523a9410595b2af0d690d82f85d5c
Paulo Rodrigues <pr.rodriguessrp@gmail.com>
2021-05-02 21:33:52 -0300

    setted controllers for new, edit, update and destroy and style bootstrap buttons

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,8 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[ show edit update destroy ]
+
   def index
-    @flats = Flat.all
+        @flats = Flat.all
 
       @markers = @flats.geocoded.map do |flat|
       {
@@ -10,4 +12,58 @@ class FlatsController < ApplicationController
       }
     end
   end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def edit
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    respond_to do |format|
+      if @flat.save
+        format.html { redirect_to @flat, notice: "Flat was successfully created." }
+        format.json { render :show, status: :created, location: @flat }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @flat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @flat.update(flat_params)
+        format.html { redirect_to @flat, notice: "Flat was successfully updated." }
+        format.json { render :show, status: :ok, location: @flat }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @flat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    respond_to do |format|
+      format.html { redirect_to flats_url, notice: "Flat was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_flat
+      @flat = Flat.find(params[:id])
+    end
+
+    def flat_params
+      params.require(:flat).permit(:name, :address)
+    end
 end
